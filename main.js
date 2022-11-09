9f9d2b1580f3550c803199cf2da7ab95490cf123
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-11-09 10:24:54 -0500

    Complete assignment requirements.

--- main.js
+++ main.js
@@ -49,7 +49,9 @@ while (count <= gridWidth * gridWidth) {
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
 
-
+let brush = document.querySelector('.current-brush');
+let palette = document.querySelectorAll('.palette div');
+let canvasSquares = document.querySelector('.canvas')
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
@@ -60,6 +62,58 @@ while (count <= gridWidth * gridWidth) {
 // empty at first, though a console.log just to know they're being
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
+let isMouseDown = false;
+
+// // this sets isMouseDown to false when pointer leaves document
+// document.addEventListener('mouseleave', () => {
+//   isMouseDown = false;
+//   console.log(isMouseDown);
+// })
+
+
+brush.addEventListener('click', () => {
+  console.log(brush.classList[1]);
+})
+
+palette.forEach(item => {
+  item.addEventListener('click', () => {
+    brush.classList.replace(brush.classList[1], item.classList[1]);
+    console.log(brush.classList[1]);
+  })
+})
+
+function canvasClick() {
+  canvasSquares.addEventListener('mousedown', function (event) {
+    isMouseDown = false;
+    event.target.classList.replace(event.target.classList[1], brush.classList[1]);
+  })
+}
+canvasClick();
+
+
+
+document.addEventListener('mousedown', () => {
+  console.log('Mouse is down!')
+  isMouseDown = true;
+})
+
+document.addEventListener('mouseup', () => {
+  console.log('Mouse is up!')
+  isMouseDown = false;
+})
+
+
+
+function canvasMouseover() {
+
+  canvasSquares.addEventListener('mouseover', function (event) { // MDN: mouseenter does not bubble
+    if (isMouseDown) {
+      event.target.classList.replace(event.target.classList[1], brush.classList[1]);
+    }
+  })
+
+}
+canvasMouseover();
 
 
 
